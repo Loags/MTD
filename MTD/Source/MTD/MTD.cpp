4e05130d85950c3e915eb75a34e90995c87824a8
Loags <35934353+Loags@users.noreply.github.com>
2024-11-08 15:38:27 +0100

    feat(anim):
    - added first person character mesh and anim for multiplayer

--- MTD/Source/MTD/MTD.cpp
+++ MTD/Source/MTD/MTD.cpp
@@ -6,3 +6,16 @@
 IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, MTD, "MTD");
 
 DEFINE_LOG_CATEGORY(MTDLog);
+
+void DrawLineBetweenActors(AActor* Actor1, AActor* Actor2, UWorld* World, FColor LineColor, float LineThickness, float Duration)
+{
+	if (Actor1 && Actor2 && World)
+	{
+		// Get the locations of the two actors
+		FVector StartPos = Actor1->GetActorLocation();
+		FVector EndPos = Actor2->GetActorLocation();
+
+		// Draw a debug line between the two positions
+		DrawDebugLine(World, StartPos, EndPos, LineColor, true, Duration, 0, LineThickness);
+	}
+}
--- MTD/Source/MTD/MTD.h
+++ MTD/Source/MTD/MTD.h
@@ -5,3 +5,6 @@
 #include "CoreMinimal.h"
 
 DECLARE_LOG_CATEGORY_EXTERN(MTDLog, Log, All);
+
+void DrawLineBetweenActors(AActor* Actor1 = nullptr, AActor* Actor2 = nullptr, UWorld* World = nullptr, FColor LineColor = FColor::Red, float LineThickness = 2.0f, float Duration = -1.0f);
+
