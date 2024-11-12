206dc07a42e8a88d57a4f6102d53101d5256d231
Loags <35934353+Loags@users.noreply.github.com>
2024-11-12 13:49:52 +0100

    feat(enemy):
    - fixed enemy movement along path / synced with client
    - added enemy health bar / synced with client

--- MTD/Source/MTD/MTD.Build.cs
+++ MTD/Source/MTD/MTD.Build.cs
@@ -4,10 +4,20 @@ using UnrealBuildTool;
 
 public class MTD : ModuleRules
 {
-	public MTD(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+    public MTD(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
-	}
+        PublicDependencyModuleNames.AddRange(new string[]
+        { 
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "InputCore",
+            "EnhancedInput",
+            "AIModule",
+            "NavigationSystem",
+            "UMG"
+        });
+    }
 }
